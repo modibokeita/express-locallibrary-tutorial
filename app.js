4f58d103dd5d3790bc5536f1c2aa696f8cece317
modibo keita <93909081keita@gmail.com>
2024-03-04 01:40:12 +0200

    First version of application moved into GitHub

---
+++ app.js
@@ -0,0 +1,70 @@
+var createError = require('http-errors');
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+
+// routes
+var indexRouter = require('./routes/index');
+var usersRouter = require('./routes/users');
+const catalogRouter = require("./routes/catalog");
+const compression = require("compression");
+const helmet = require("helmet");
+// Set up rate limiter: maximum of twenty requests per minute
+const RateLimit = require("express-rate-limit");
+// Database settings
+var app = express();
+const mongoose = require('mongoose');
+mongoose.set('strictQuery', false);
+const dev_db_url = "mongodb+srv://keita:93909081Ke@cluster0.0q5ec20.mongodb.net/local_library?retryWrites=true&w=majority&appName=Cluster0"
+const mongoDB = process.env.MONGODB_URI || dev_db_url;
+main().catch((err) => console.log(err));
+async function main() {
+  await mongoose.connect(mongoDB);
+}
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'pug');
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(compression()); // Compress all routes
+const limiter = RateLimit({
+  windowMs: 1 * 60 * 1000, // 1 minute
+  max: 20,
+});
+// Apply rate limiter to all requests
+app.use(limiter)
+// Add helmet to the middleware chain.
+// Set CSP headers to allow our Bootstrap and Jquery to be served
+app.use(
+  helmet.contentSecurityPolicy({
+    directives: {
+      "script-src": ["'self'", "code.jquery.com", "cdn.jsdelivr.net"],
+    },
+  }),
+);
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+app.use("/catalog", catalogRouter);
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  next(createError(404));
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+module.exports = app;
---
+++ models/author.js
@@ -0,0 +1,34 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+const { DateTime } = require("luxon");
+const AuthorSchema =  new Schema({
+    first_name: {type: 'string', required: true, maxLength: 100},
+    family_name: {type: 'string', required: true, maxLength: 100},
+    date_of_birth: {type: Date},
+    date_of_death: {type: Date},
+});
+// Virtual for author's full name
+AuthorSchema.virtual("name").get(function (){
+    // To avoid errors in cases where an author does not have either a family name or first name
+    // We want to make sure we handle the exception by returning an empty string for that case
+
+    let fullname = "";
+
+    if (this.family_name && this.first_name) {
+        fullname = `${this.family_name} ${this.first_name}`;
+    }
+    return fullname;
+})
+// Virtual for author's URL
+AuthorSchema.virtual("url").get(function (){
+    // We don't use an arrow function as we'll need the this object
+    return `/catalog/author/${this._id}`;
+
+});
+AuthorSchema.virtual('lifespan').get(function () {
+    const birthFormatted = DateTime.fromJSDate(this.date_of_birth).toFormat('LLL d, yyyy');
+    const deathFormatted = DateTime.fromJSDate(this.date_of_death).toFormat('LLL d, yyyy');
+    return `${birthFormatted} - ${deathFormatted}`;
+});
+// Export model
+module.exports = mongoose.model("Author", AuthorSchema);
